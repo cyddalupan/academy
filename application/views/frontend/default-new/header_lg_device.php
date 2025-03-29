4d5249936225d4234590cebecf15d2c41993943e
cyd dalupan <cydmdalupan@gmail.com>
2025-03-29 07:40:02 +0000

    Add new button and modal

--- application/views/frontend/default-new/header_lg_device.php
+++ application/views/frontend/default-new/header_lg_device.php
@@ -322,7 +322,12 @@
                         </a>
                       </li>  
                     <?php endif; ?>
-
+                    <li>
+                      <a href="#" data-toggle="modal" data-target="#myResultModal">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        My Result
+                      </a>
+                    </li>
                     <li>
                       <a href="<?php echo site_url('login/logout'); ?>">
                         <i class="fa-solid fa-arrow-right-from-bracket"></i>
@@ -348,3 +353,24 @@
 
   </div>
 </nav>
+
+<!-- Modal -->
+<div class="modal fade" id="myResultModal" tabindex="-1" role="dialog" aria-labelledby="myResultModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="myResultModalLabel">My Result</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <!-- Content for My Result goes here -->
+        <p>Your results will be displayed here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
