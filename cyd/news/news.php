e212ff1c42d9d3aa62976bd333cbb7517e847644
cyd dalupan <cydmdalupan@gmail.com>
2025-03-29 05:47:46 +0000

    test display

--- cyd/news/news.php
+++ cyd/news/news.php
@@ -89,6 +89,7 @@ try {
 
 <div class="container">
     <div class="header">
+		<?php echo $score_counts; ?>
         <h1>Total Average Score: <?php echo number_format($score_counts[4], 2); ?></h1>
     </div>
 
