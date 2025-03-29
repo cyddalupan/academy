94bc267b30d35baee12199c1da668b399655b55d
cyd dalupan <cydmdalupan@gmail.com>
2025-03-29 03:36:43 +0000

    iframe hello

---
+++ cyd/news/news.php
@@ -0,0 +1,18 @@
+<?php
+require '../config.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>News</title>
+</head>
+
+<body>
+    <h1>Student News Dashboard</h1>
+</body>
+
+</html>
