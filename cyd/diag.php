85d9440dd8f492aeeb43d05c4d6e8877d9c0979d
cyd dalupan <cydmdalupan@gmail.com>
2025-03-30 09:01:59 +0000

    add bootstrap

--- cyd/diag.php
+++ cyd/diag.php
@@ -118,6 +118,7 @@ try {
 <head>
 	<meta charset="UTF-8">
 	<title>Quiz Application</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<?php // require 'style.php'; ?>
 </head>
 <body> 
