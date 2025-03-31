45d53e0647be6b30172147ee2c51cc04a26e6e51
cyd dalupan <cydmdalupan@gmail.com>
2025-03-31 09:00:34 +0000

    js try

--- cyd/news/news.php
+++ cyd/news/news.php
@@ -122,7 +122,7 @@ try {
 
 	<?php require 'script.php'; ?><!-- Include Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2/dist/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 </body>
