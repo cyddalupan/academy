08a7236ed6b69b45afab4345f233543713184832
cyd dalupan <cydmdalupan@gmail.com>
2025-03-31 09:08:58 +0000

    try

--- cyd/news/news.php
+++ cyd/news/news.php
@@ -123,7 +123,14 @@ try {
 	<?php require 'script.php'; ?><!-- Include Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
+	<script>
+	document.addEventListener('DOMContentLoaded', function () {
+		var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+		var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl);
+		});
+	});
+	</script>
 </body>
 
 </html>
