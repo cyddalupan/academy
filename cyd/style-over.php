d6224f297bdf7bea561a565e352ab11952e5c419
cyd dalupan <cydmdalupan@gmail.com>
2025-04-01 14:10:09 +0000

    over

---
+++ cyd/style-over.php
@@ -0,0 +1,20 @@
+<style>
+    .btn-primary {
+    background-color: #6742e9;
+    border-color: #6742e9;
+}
+
+.alert-primary {
+    background-color: #6742e9;
+    border-color: #6742e9;
+    color: #fff; /* Adjust text color if needed */
+}
+
+.bg-primary {
+    background-color: #6742e9 !important;
+}
+
+.text-primary {
+    color: #6742e9 !important;
+}
+</style>
