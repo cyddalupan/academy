564a43f1de61db1ebcbd0e511bd17c3568f45203
cyd dalupan <cydmdalupan@gmail.com>
2025-04-01 14:16:44 +0000

    bg

--- cyd/style-over.php
+++ cyd/style-over.php
@@ -1,20 +1,28 @@
 <style>
-    .btn-primary {
-    background-color: #6742e9;
-    border-color: #6742e9;
+body {
+    font: Josefin Sans !important;
+    background-color: #606060;
+}
+.btn-primary {
+    background-color: #021487;
+    border-color: #021487;
 }
 
 .alert-primary {
-    background-color: #6742e9;
-    border-color: #6742e9;
+    background-color: #021487;
+    border-color: #021487;
     color: #fff; /* Adjust text color if needed */
 }
 
 .bg-primary {
-    background-color: #6742e9 !important;
+    background-color: #021487 !important;
 }
 
 .text-primary {
-    color: #6742e9 !important;
+    color: #021487 !important;
+}
+
+.card-header {
+    background-color: #021487 !important;
 }
 </style>
