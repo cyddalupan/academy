a2fe57b2cdbe39b383fe8733b881b1877834fd40
cyd dalupan <cydmdalupan@gmail.com>
2025-04-01 15:08:39 +0000

    fb

--- application/views/frontend/default-new/index.php
+++ application/views/frontend/default-new/index.php
@@ -75,5 +75,33 @@ if($language_dirs){
 	?>
 
 	<?php echo get_frontend_settings('embed_code'); ?>
+
+	<!-- FB -->
+	<div id="fb-root"></div>
+	<div id="fb-customer-chat" class="fb-customerchat"></div>
+
+	<script>
+	var chatbox = document.getElementById('fb-customer-chat');
+	chatbox.setAttribute("page_id", "607142962480605");
+	chatbox.setAttribute("attribution", "biz_inbox");
+	</script>
+
+	<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+		xfbml            : true,
+		version          : 'v21.0' // Use the desired API version here
+		});
+	};
+
+	(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
+
 </body>
 </html>
