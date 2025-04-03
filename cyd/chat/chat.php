80d8c1ef4d8f72556df65d86ccce17d0d37de85f
cyd dalupan <cydmdalupan@gmail.com>
2025-04-03 14:42:19 +0000

    cache

--- cyd/chat/chat.php
+++ cyd/chat/chat.php
@@ -1,6 +1,12 @@
 <?php
 require '../config.php';
 
+if (ENV == "dev") {
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+}
+
 $apiKey = OPEN_AI;
 
 try {
