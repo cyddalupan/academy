d2dd2394f754c1852dc7f6e030372ba3beb1da3d
cyd dalupan <cydmdalupan@gmail.com>
2025-09-08 03:00:18 +0000

    no logs

--- cyd/api/lawgpt_premium.php
+++ cyd/api/lawgpt_premium.php
@@ -124,7 +124,6 @@ try {
         JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES
     );
 } catch (Exception $e) {
-    error_log('Caught exception: ' . $e->getMessage());
     http_response_code(500);
     echo json_encode(['error' => $e->getMessage()]);
 }
