3e36efd24e23d91898e0147e154dd63bc764899e
cleddrence <cydmdalupan@gmail.com>
2025-10-04 08:25:19 +0000

    try 2

--- application/views/frontend/default-new/login.php
+++ application/views/frontend/default-new/login.php
@@ -14,7 +14,7 @@
                     <h3><?php echo get_phrase('Log In'); ?><span>!</span></h3>
                     <p><?php echo get_phrase('Explore, learn, and grow with us. Enjoy a seamless and enriching educational journey. Lets begin!') ?></p>
 
-                    <form action="<?php echo site_url('login/validate_login') ?>" method="post" id="login-form">
+                    <form action="<?php echo site_url('login/validate_login') ?>" method="post" id="login-form" target="_top">
                         <?php if (isset($is_iframe) && $is_iframe): ?>
                             <input type="hidden" name="iframe" value="true">
                         <?php endif; ?>
