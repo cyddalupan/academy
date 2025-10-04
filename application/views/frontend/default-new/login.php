7479e752cdb9cf338de80aabcac695329434ddcc
cleddrence <cydmdalupan@gmail.com>
2025-10-04 08:53:08 +0000

    try 7

--- application/views/frontend/default-new/login.php
+++ application/views/frontend/default-new/login.php
@@ -1,7 +1,3 @@
-<script>
-  alert("Login page script is running");
-</script>
-
 <?php if(get_frontend_settings('recaptcha_status')): ?>
   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <?php endif; ?>
@@ -86,20 +82,16 @@
     }
 
     function handleLogin() {
-      alert("handleLogin called");
       var form = $('#login-form');
-      alert("Form data: " + form.serialize());
       $.ajax({
         type: "POST",
         url: form.attr('action'),
         data: form.serialize(),
         success: function(response) {
-          alert("AJAX success. Response: " + response);
           distributeServerResponse(response);
         },
         error: function(xhr, status, error) {
-          alert("AJAX error. Status: " + status + ". Error: " + error);
-          alert("Response text: " + xhr.responseText);
+          alert("An error occurred during login.");
         }
       });
     }
--- application/views/frontend/default-new/mobilegpt.php
+++ application/views/frontend/default-new/mobilegpt.php
@@ -1,3 +1,7 @@
+<script>
+  alert("User ID from local storage: " + localStorage.getItem('user_id'));
+</script>
+
 <chatbot-widget></chatbot-widget>
 
 <style type="text/css">
